fddff401fc05e82da0516af851fe2376df8d1c91
Francy Salgado <francystellasalgado@gmail.com>
2021-07-05 22:20:02 -0500

    Se crea diagrama de clases y proyecto de la tienda

---
+++ tienda/src/Producto.java
@@ -0,0 +1,11 @@
+public class Producto {
+    
+    private String nombre = "";
+    private float precio = 0;
+
+    public void mostrar(){
+        System.out.println(nombre + ": " + precio);
+
+    }
+
+}
