eec882e19356fe79d7488b6e772f866a11a10f9f
Francy Salgado <francystellasalgado@gmail.com>
2021-07-07 08:20:21 -0500

    Se crea la clase producto y su respectivo objeto

--- tienda/src/App.java
+++ tienda/src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("La Tienda de Ropa");
+
+        Producto productoUno = new Producto(); //igual al nombre del constructor que se creo en prducto.java
+//puedo crear varios productos
+        productoUno.setNombre("Falda");
+        productoUno.setPrecio(70000);
+        productoUno.mostrar(); // es la instruccion de la linea 11 de producto.java
+
+        
     }
 }
--- tienda/src/Producto.java
+++ tienda/src/Producto.java
@@ -3,9 +3,26 @@ public class Producto {
     private String nombre = "";
     private float precio = 0;
 
-    public void mostrar(){
-        System.out.println(nombre + ": " + precio);
+    public Producto() {
 
     }
 
+    public void mostrar() {
+        String precioTexto = String.format("%,d", (int)precio); //esta linea da formato al valor para separar los miles
+        System.out.println(nombre + ": " + precioTexto);
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre; // this es un señalador, hace referencia que le pertenece asi mismo
+    }
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+    public float getPrecio() {
+        return this.precio;
+    }
 }
