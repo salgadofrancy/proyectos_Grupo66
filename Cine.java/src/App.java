c01ff7dda81994ec597e4eecf13220038c1c1959
Francy Salgado <francystellasalgado@gmail.com>
2021-07-15 08:57:23 -0500

    actualizacion del contenido de  Prueba

--- Cine.java/src/App.java
+++ Cine.java/src/App.java
@@ -28,19 +28,53 @@ public class App {
         
         // cliente Dos
         ClientePremium clienteDos = new ClientePremium();
-        clienteDos.setCedula("12345765");
+        clienteDos.setCedula("12345789");
         clienteDos.setNombre("José");
         clienteDos.setNumeroTarjeta(100);
         clienteDos.mostrarDatos();
         float costoDos = clienteDos.calcularCosto(8000);
 
         System.out.println();
-        System.out.println("Cédula:  " + clienteDos.getCedula() + "\nNombre: "
+        System.out.println("Cédula: " + clienteDos.getCedula() + "\nNombre: "
         + clienteDos.getNombre() + "\nNúmero tarjeta: " + clienteDos.getNumeroTarjeta()
         + "\nCosto tiquete: " + costoDos);
 
 
+        //prueba de contenedores
+        System.out.println();
+        Prueba pruebaContenedor = new Prueba();
+        pruebaContenedor.generarLista();
+     
+        //Lista de objetos. creamos un objeto de tipo cliente, para luego pasarle esos datos a la lista
+        System.out.println("CEACION DE CLIENTES");
+        String opcion = "S";
+        while (opcion.equalsIgnoreCase("S")) { // Ignora mayúsculas
+            //Ingresa datos por teclado
+            System.out.println("Ingrese su cédula:");
+            cedula = sca.next(); // Uso de next
+            sca.nextLine();
+            System.out.println("Ingrese su nombre:");
+            nombre = sca.nextLine();
+            System.out.println("Ingrese su ID:");
+            int id = sca.nextInt();
+            
+            // crea objeto cliente
+            Cliente nuevoCliente = new Cliente();
+            nuevoCliente.setCedula(cedula);
+            nuevoCliente.setNombre(nombre);
+            nuevoCliente.setId(id);
+            
+            //Agrega cliente a la lista
+            pruebaContenedor.agregarCliente(nuevoCliente);
+            
+            //Preguntar nuevo cliente
+            System.out.println("Desea agregar otro cliente? (s / n):");
+            opcion = sca.next();
+            sca.nextLine();
+        }
 
+        pruebaContenedor.mostrarNombreClientes();
+        pruebaContenedor.mostrarDatosClientes();
 
     }
 }
---
+++ Cine.java/src/Prueba.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Prueba {
+
+    private ArrayList<Cliente> listaClientes; //aqui se declara y se crea aparte con metodos o constructor
+
+    //agregamos un constructor, sin  void
+    public Prueba(){
+        listaClientes = new ArrayList<>();
+    }
+//agregamos metodo, 
+public void agregarCliente(Cliente pCliente){// dentro del parentesis va un objeto ya construido, para diferenciarlo agrega una p como parametro 
+    listaClientes.add(pCliente);
+    
+}
+
+public void mostrarNombreClientes(){
+    System.out.println("Nombre de Clientes");
+    for (int i = 0; i< listaClientes.size(); i++){
+        System.out.println(listaClientes.get(i).getNombre());
+
+    }
+}
+public void mostrarDatosClientes() {
+    String cadenaClientes = "";
+    System.out.println("Datos de clientes");
+    for (int i = 0; i < listaClientes.size(); i++){
+        String datos = "\nCédula: " + listaClientes.get(i).getCedula()
+                        + "\nNombre: " + listaClientes.get(i).getNombre()
+                        + "\nID: " + listaClientes.get(i).getId();
+        cadenaClientes = cadenaClientes.concat(datos);
+    }
+    System.out.println(cadenaClientes);
+}
+
+    public void generarLista(){
+        //System.out.println("Ingreso al metodo prueba"); // aqui se prueba si esta funcionando el metodo
+        ArrayList<Integer> listaNumeros = new ArrayList<>();
+
+        Scanner sca = new Scanner(System.in);
+        int numero = 0;
+        while (numero >=0){
+        System.out.println("Ingrese un número, y para salir digite un (Número negativo para salir)");
+        numero = sca.nextInt();
+        if(numero >=0){
+            listaNumeros.add(numero);
+            }
+        }
+        System.out.println();
+
+        //ciclo for
+        for (int i = 0; i < listaNumeros.size(); i++) {
+            System.out.println(listaNumeros.get(i));
+        }
+
+        System.out.println("_____");
+
+        //ciclo for each
+        listaNumeros.forEach((elemento) -> {
+            System.out.println(elemento);
+            //elemento = elemento *2;
+        });
+    }
+    
+}
